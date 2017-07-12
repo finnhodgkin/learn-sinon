b3b9e877d00bb8b884109a032b68f594e0ff6a2d
Zooey <zooey@lbk.org.uk>
2017-07-12 16:38:04 +0100

    adds first test uses sinon to stub request.get and tests functionality #2

--- giphy.js
+++ giphy.js
@@ -1,14 +1,18 @@
 const request = require('request');
 
 function getImage(searchParam, callback) {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${searchParam}&limit=1&api_key=dc6zaTOxFJmzC`
-  request(url, (err, res, body) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${searchParam}&limit=1&api_key=dc6zaTOxFJmzC`;
+  request.get(url, (err, res, body) => {
     const imageUrl = JSON.parse(body).data[0].images.original.url;
     callback(null, imageUrl);
   });
 }
 
-getImage('hello', (err, result) => {
-  if(err) return console.log('ERROR: ', err);
-  console.log(result);
-})
+// getImage('hello', (err, result) => {
+//   if (err) return console.log('ERROR: ', err);
+//   console.log(result);
+// });
+
+module.exports = {
+  getImage,
+};
---
+++ test.spec.js
@@ -0,0 +1,29 @@
+const test = require('tape');
+const sinon = require('sinon');
+const sinonTest = require('sinon-test');
+const giphy = require('./giphy.js');
+const request = require('request');
+
+test('tests getImage', t => {
+  var pretendReturn = {
+    data: [
+      {
+        images: {
+          original: {
+            url: 'i am a url',
+          },
+        },
+      }
+    ]
+  }
+  // stub request.get
+  var stub = sinon.stub(request, 'get');
+  // calling stub.yields calls the first function passed to the stubbed function when that function is called, with the arguments supplied to stub.yields passed to that callback
+  stub.yields(null, null, JSON.stringify(pretendReturn));
+  // so we call getImage, which does a request.get and then calls the function passed into getImage inside of the callback to request.get (and that callback will be called with the arguments passed to yield). So, we can do our t.equal inside of the callback to getImage, knowing what will be passed into the function that calls it as we have called stub.yield on it
+  giphy.getImage('cats', (err, res) => {
+    t.equal(res, "i am a url", "tests that when the callback to request gets the expected argument it works properly")
+    t.end();
+  });
+
+});
