7b05f3354130a53d651b64a3f1c4c065ff717f9c
finnhodgkin <finnhodgkin@gmail.com>
2017-07-12 17:04:22 +0100

    stubs the api call for some error handling. #2

--- giphy.js
+++ giphy.js
@@ -3,15 +3,19 @@ const request = require('request');
 function getImage(searchParam, callback) {
   const url = `https://api.giphy.com/v1/gifs/search?q=${searchParam}&limit=1&api_key=dc6zaTOxFJmzC`;
   request.get(url, (err, res, body) => {
-    const imageUrl = JSON.parse(body).data[0].images.original.url;
+    const parsedBody = body ? JSON.parse(body) : undefined;
+    if (!parsedBody ||
+        !parsedBody.data ||
+        !parsedBody.data[0] ||
+        !parsedBody.data[0].images ||
+        !parsedBody.data[0].images.original ||
+        !parsedBody.data[0].images.original.url) {
+      return callback(new Error('Sorry, no results.'));
+    }
+    const imageUrl = parsedBody.data[0].images.original.url;
     callback(null, imageUrl);
   });
 }
-
-// getImage('hello', (err, result) => {
-//   if (err) return console.log('ERROR: ', err);
-//   console.log(result);
-// });
 
 module.exports = {
   getImage,
--- test.spec.js
+++ test.spec.js
@@ -5,7 +5,8 @@ const giphy = require('./giphy.js');
 const request = require('request');
 
 test('tests getImage', t => {
-  var pretendReturn = {
+  t.plan(3);
+  const pretendReturn = {
     data: [
       {
         images: {
@@ -17,13 +18,31 @@ test('tests getImage', t => {
     ]
   }
   // stub request.get
-  var stub = sinon.stub(request, 'get');
-  // calling stub.yields calls the first function passed to the stubbed function when that function is called, with the arguments supplied to stub.yields passed to that callback
+  const stub = sinon.stub(request, 'get');
+  // calling stub.yields calls the first function passed to the stubbed function
+  // when that function is called, with the arguments supplied to stub.yields
+  // passed to that callback
   stub.yields(null, null, JSON.stringify(pretendReturn));
-  // so we call getImage, which does a request.get and then calls the function passed into getImage inside of the callback to request.get (and that callback will be called with the arguments passed to yield). So, we can do our t.equal inside of the callback to getImage, knowing what will be passed into the function that calls it as we have called stub.yield on it
+  // so we call getImage, which does a request.get and then calls the function
+  // passed into getImage inside of the callback to request.get (and that
+  // callback will be called with the arguments passed to yield). So, we can do
+  // our t.equal inside of the callback to getImage, knowing what will be passed
+  // into the function that calls it as we have called stub.yield on it
   giphy.getImage('cats', (err, res) => {
     t.equal(res, "i am a url", "tests that when the callback to request gets the expected argument it works properly")
-    t.end();
   });
 
+  stub.reset();
+  stub.yields(null, null, undefined);
+
+  giphy.getImage('cats', (err, res) => {
+    t.ok(err, 'tests that undefined body responses are error handled');
+  })
+
+  stub.reset();
+  stub.yields(null, null, JSON.stringify({data:[]}));
+
+  giphy.getImage('cats', (err, res) => {
+    t.ok(err, 'tests that bad body responses are are error handled');
+  })
 });
